f0fafa5512ec3e99be1fda50029c9f90db4096ec
binod164 <binod7_grg@hotmail.com>
2022-04-22 18:27:50 -0500

    complete questions 5 and 6

--- practice.js
+++ practice.js
@@ -7,7 +7,6 @@ function binToDec(bin){
 //2.Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 function solution(str, ending){
-  // TODO: complete
   if(ending.length < 1){
     return true;
   }
@@ -61,3 +60,62 @@ function coinCombo (cents) {
   return coins;
 }
 
+// 5.Kata Task
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  
+  let years =  [0,0,0];
+  years[0] = humanYears;
+  if(humanYears === 1){
+    years[1] = 15
+    years[2] = 15   
+  }else if(humanYears === 2){
+    years[1] = 15 + 9;
+    years[2] = 15 + 9;
+  }else{
+    years[1] = 15 + 9 + (4 * (humanYears -2));
+    years[2] = 15 + 9 + (5 * (humanYears -2));
+  }
+  return years;
+}
+
+// 6.Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+function squareOrSquareRoot(array) {
+  let newArr = [];
+  for(let i=0; i<array.length; i++){
+    if(Math.sqrt(array[i]) === Math.floor(Math.sqrt(array[i]))){
+      newArr.push(Math.sqrt(array[i]));
+    }else{
+      newArr.push(Math.pow(array[i],2));
+    }
+  }
+  return newArr;  
+}
