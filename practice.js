8ed83d5671dd36045ca59107422dc09faa53691b
binod164 <binod7_grg@hotmail.com>
2022-05-10 16:26:57 -0500

    Solve Q 46

--- practice.js
+++ practice.js
@@ -817,3 +817,24 @@ function diagonalDifference(arr) {
 
 }
 
+//46.There is a sequence of words in CamelCase as a string of letters, , having the following properties:
+
+// It is a concatenation of one or more words consisting of English letters.
+// All letters in the first word are lowercase.
+// For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+// Given , determine the number of words in .
+
+function camelcase(s) {
+  // Write your code here
+  let letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let count = 1;
+  
+  for(let i=0; i<s.length; i++){
+      if(letters.includes(s[i])){
+          count++;
+      }     
+  }
+  console.log(count)
+  return count;
+}
+
