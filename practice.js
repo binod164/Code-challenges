a8c22c11f656b50aceee3e16416047689acda612
binod164 <binod7_grg@hotmail.com>
2022-05-10 21:06:34 -0500

    Solve Q 47

--- practice.js
+++ practice.js
@@ -838,3 +838,19 @@ function camelcase(s) {
   return count;
 }
 
+//47.You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+var maximumWealth = function(accounts) {
+  maxWealth = 0;
+  let wealth = 0;
+  for(let i=0; i<accounts.length; i++){
+      for(let j=0; j<accounts[i].length; j++){
+          wealth += accounts[i][j];
+      }
+      if(wealth > maxWealth){
+          maxWealth = wealth;
+      }
+      wealth = 0;
+  }
+  return maxWealth;
+};
