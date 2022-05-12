70d29651332ae24e6940ed43be0378b26b416795
binod164 <binod7_grg@hotmail.com>
2022-05-12 00:08:19 -0500

    Solve Q 52

--- practice.js
+++ practice.js
@@ -944,3 +944,13 @@ var romanToInt = function(s) {
   }
   return total;
 };
+
+//52.Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+let reverseStr = function(s, k) {
+  for(let i = 0; i < s.length; i+= k*2) {
+    let subStr = s.slice(i, i + k);
+    s = s.replace(subStr, subStr.split('').reverse().join(''))
+  }
+  return s;
+};
