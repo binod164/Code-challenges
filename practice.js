6585b301b4fd51d9ab628645c762dd9f1d7a8c69
binod164 <binod7_grg@hotmail.com>
2022-05-11 22:43:51 -0500

    Solve Q49

--- practice.js
+++ practice.js
@@ -870,3 +870,18 @@ var runningSum = function(nums) {
   return newArr;
   
 };
+
+//49.Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.Return k after placing the final result in the first k slots of nums.
+
+var removeElement = function(nums, val) {
+  let j = 0;
+  for(let i=0; i<nums.length; i++){
+      if(nums[i] !== val){
+          nums[j] = nums[i];
+          j++;
+      }
+  }
+  return j;
+};
+
+//50.
