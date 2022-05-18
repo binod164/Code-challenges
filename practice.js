3c3a4d01ac93ddb09f1dbbb106ef30db80383c6a
binod164 <binod7_grg@hotmail.com>
2022-05-18 18:37:11 -0500

    Solve Q 56

--- practice.js
+++ practice.js
@@ -1028,3 +1028,21 @@ for(let i = 0; i< str.length ; i++){
 }
 return result;
 }
+
+//56.- Write a function named fromPairs that creates an object from an array containing nested arrays.
+// - Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+// - If a key appears in multiple pairs, the rightmost pair should overwrite the previous entry in the object.
+
+// Examples:
+
+// fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+// fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+// Your solution for 14-fromPairs here:
+
+function fromPairs(arr){
+  let obj = {};
+  arr.forEach(keyValueArray=>{
+    obj[keyValueArray[0]] = keyValueArray[1];
+  });
+  return obj;
+}
