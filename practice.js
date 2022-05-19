bbf3c52e3e6af08c4962e1708cd38f2cf44c5ad1
binod164 <binod7_grg@hotmail.com>
2022-05-19 11:56:43 -0500

    Solve Q 58

--- practice.js
+++ practice.js
@@ -1073,3 +1073,37 @@ function flatten(arr){
   });
   return newArr;
 }
+
+
+//58.Prompt:
+// - Write a function named primeFactors that accepts a whole number greater than one (1) as an argument and returns an array of that argument's prime factors.
+// - The prime factors of a whole number are the prime numbers that, when multiplied together, equals the whole number.
+// - If the argument provided is not greater than 1, or not a whole number, then primeFactors should return an empty array.
+
+// Examples:
+
+// primeFactors(2) //=> [2]
+// primeFactors(3) //=> [3]
+// primeFactors(4) //=> [2, 2]
+// primeFactors(18) //=> [2, 3, 3]
+// primeFactors(29) //=> [29]
+// primeFactors(105) //=> [3, 5, 7]
+// primeFactors(200) //=> [2, 2, 2, 5, 5]
+
+function primeFactors(num){
+  let newArr = [];
+  if (num < 2 || !Number.isInteger(num)){
+    return newArr;
+  }
+  let divisor = 2;
+    while (num >= divisor * divisor) {
+      if (Number.isInteger(num / divisor)) {
+        newArr.push(divisor);
+        num = num / divisor;
+      } else {
+        divisor++;
+      }
+    }
+    newArr.push(num);
+    return newArr;
+}
