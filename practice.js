4fd6f888c5c081544baf84f82fc242c3be744bf2
binod164 <binod7_grg@hotmail.com>
2022-05-22 20:22:01 -0500

    Solve Q 65

--- practice.js
+++ practice.js
@@ -1246,3 +1246,40 @@ function toCamelCase(str) {
     return match.charAt(1).toUpperCase();
   });
 }
+
+//65.- Write a function named findHighestPriced that accepts a single array of objects.
+// - The objects contained in the array are guaranteed to have a price property holding a numeric value.
+// - The function should return the object in the array with the largest value held in the price property.
+// - If there's a tie between two or more objects, return the first of those objects in the array.
+// - Return the original object, not a copy.
+// - Do not mutate the array that is passed in.
+
+// Examples:
+
+// findHighestPriced([
+//   { sku: 'a1', price: 25 },
+//   { sku: 'b2', price: 5 },
+//   { sku: 'c3', price: 50 },
+//   { sku: 'd4', price: 10 }
+// ]);
+//=> { sku: 'c3', price: 50 } 
+
+// findHighestPriced([
+//   { sku: 'a1', price: 25 },
+//   { sku: 'b2', price: 50 },
+//   { sku: 'c3', price: 50 },
+//   { sku: 'd4', price: 10 }
+// ]);
+//=> { sku: 'b2', price: 50 }
+
+function findHighestPriced(arr){
+  let largestValue = 0;
+  let result = 0;
+  arr.forEach(obj => {
+    if(obj.price > largestValue){
+      largestValue = obj.price;
+      result = obj;
+    }
+  });
+  return result;
+}
