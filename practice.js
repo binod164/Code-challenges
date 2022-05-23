92f443d1827cd4b12bad374cd7fac9ffc3abe569
binod164 <binod7_grg@hotmail.com>
2022-05-23 17:57:49 -0500

    Solve Q 67

--- practice.js
+++ practice.js
@@ -1310,3 +1310,37 @@ function mapArray(arr, func){
   });
   return newArr;
 }
+
+
+//67.- This challenge uses an imaginary grid where the y coordinate increases when you move up and decreases when you move down. Similarly, the x coordinate increases when you move right and decreases when you move left.
+// - Write a function called gridTrip that accepts two arguments.
+// - The first argument is an array containing two integers. The value in the first index of the array represents the starting x position on the grid. The second value in the array represents the starting y position.
+// - The second argument is a string representing "moves" using the characters 'U', 'D', 'R' & 'L' to mean moving Up, Down, Right & Left, respectively. Each direction character is followed by digits representing how many units to move in that direction. For example, a string of 'D15R2U4' moves up 15 units, right 2 units, and finally, down 4 units from the starting coordinates.
+// - The direction characters will always be upper case.
+// - The gridTrip function should return a new array of two integers: the final x position and the final y position. Do not modify the array argument).
+
+// Hint:
+// - Using the String.match method to return an array of regular expression matches can be helpful if you want to break the single string of moves into an array of distinct moves by direction. Be sure to use the global flag, e.g. /cat/g, when defining the regexp.
+
+// Examples:
+
+// gridTrip( [0, 0], 'U2R1' ) // => [1, 2]
+// gridTrip( [10, 5], 'D5L15U2' ) //-> [-5, 2]
+// gridTrip( [100, -22], 'L2L15D50U1D9') //=> [83, -80]
+
+function gridTrip(xyArr, moves) {
+  let result = [xyArr[0], xyArr[1]];
+  const lookup = {'R': [0, 1], 'U': [1, 1], 'L': [0, -1], 'D': [1, -1]};
+  let idx = 0;
+  while (idx < moves.length) {
+    let dir = moves[idx];
+    idx++;
+    let numString = '';
+    while ('0123456789'.includes(moves[idx]) && idx < moves.length) {
+      numString += moves[idx];
+      idx++;
+    }
+    result[lookup[dir][0]] += numString * lookup[dir][1];
+  }
+  return result;
+}
