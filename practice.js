061a4dc1182658f85e9c644fef4279198660d897
binod164 <binod7_grg@hotmail.com>
2022-05-23 17:59:01 -0500

    Solve Q 68

--- practice.js
+++ practice.js
@@ -1344,3 +1344,26 @@ function gridTrip(xyArr, moves) {
   }
   return result;
 }
+
+//68.- Write a function called countTheBits that accepts a single numeric argument that will be an integer.
+// - The function should return the number of bits set to 1 in the number's binary representation.
+
+// Hints:
+
+// - We typically work with "decimal" numbers daily. Decimal is "base 10", where there are 10 digits available - 0 thru 9. However, it's binary that computers understand - 1's and 0's. The 1's and 0's are called bits.
+// - As an example, the decimal value of 13 is represented in binary as 1101. There are 3 one bits and 1 zero bit in the decimal number of 13.
+// - Carefully read the documentation for the Number.prototype.toString method.
+
+// Examples:
+
+// countTheBits( 0 ) // => 0
+// countTheBits( 13 ) // => 3
+// countTheBits( 256 ) // => 1
+// countTheBits( 255 ) //=> 8
+// countTheBits( 65535 ) //=> 16
+
+function countTheBits(int){
+  let binary_val = int.toString(2)
+  let one_bits = binary_val.split("").filter(bit => bit == '1')
+  return one_bits.length
+}
