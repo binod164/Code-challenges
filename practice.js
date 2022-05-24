77c1be4136ef6bf4b8fcc9d36e077edacf747354
binod164 <binod7_grg@hotmail.com>
2022-05-24 13:16:01 -0500

    Solve Q 70

--- practice.js
+++ practice.js
@@ -1394,3 +1394,23 @@ function getNumForIP(str) {
   });
   return sum;
 }
+
+//70.A peak element is an element that is strictly greater than its neighbors.
+
+// Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞.
+
+// You must write an algorithm that runs in O(log n) time.
+
+var findPeakElement = function(nums) {
+  if (nums.length == 1) return 0;
+  if (nums[0] >= nums[1]) return 0;
+ if (nums[nums.length - 1] >= nums[nums.length - 2]) return nums.length - 1;
+for (let i = 1; i< nums.length;i++){
+ if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+     return i;
+ }
+}
+
+};
