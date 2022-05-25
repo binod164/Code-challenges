b928bc8465552a5f823d5e7911f14d732a3df618
binod164 <binod7_grg@hotmail.com>
2022-05-25 16:08:11 -0500

    Solve Q 74

--- practice.js
+++ practice.js
@@ -1487,3 +1487,36 @@ var reverse = function(x) {
   }
   return isNegative ? result*-1 : result;
 };
+
+//74.Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+
+let result = true;
+var isHappy = function(n) {
+    let sum = sumOfDigits(n);
+    if (sum === 1 || n === 1111111) {
+        result = true;
+    } else if (sum > 1 && sum < 10) {
+        result = false;
+    } else {
+        isHappy(sum);
+    }
+    return result;
+};
+
+function sumOfDigits(num) {
+    let sum = 0;
+    for (let i = num; i >= 1; i = Math.floor( i / 10)) {
+        let singleNum = i % 10;
+        let sq = singleNum * singleNum;
+        sum += sq;
+    }
+    return sum;
+}
